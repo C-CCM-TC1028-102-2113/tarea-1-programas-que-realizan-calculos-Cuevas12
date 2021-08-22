559eaf0e49ac8571bc3898d8b435e8c9698e4e32
Cuevas12 <89288980+Cuevas12@users.noreply.github.com>
2021-08-22 16:38:28 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,8 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
-    #Leer los datos
+    men=int(input(" Dame el número de mensajes: "))
+    meg=float(input("Dame el número de megas: "))
+    nmi=int(input("Dame el número de minutos: "))
+    qmen=men*0.8
+    qmeg=meg*0.8
+    qnmi=nmi*0.8
+    cm=qmen+qmeg+qnmi
+    print("El costo mensual es: ",cm)
     pass
-
 
 if __name__ == '__main__':
     main()
+
